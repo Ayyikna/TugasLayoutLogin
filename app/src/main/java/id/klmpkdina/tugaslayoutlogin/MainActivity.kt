7b6d35c5dbe5be6953ad3791f72f8fb4d0578c5b
AYYI <sayyidahathallah4930@gmail.com>
2024-05-05 12:08:09 +0700

    Initial commit

---
+++ app/src/main/java/id/klmpkdina/tugaslayoutlogin/MainActivity.kt
@@ -0,0 +1,30 @@
+package id.klmpkdina.tugaslayoutlogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+
+        findViewById<Button>(R.id.btnRegister).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
